96080951332a452c9b18d832e6d3ea4c678545c2
lxyvelyf <2927699121@qq.com>
2023-10-18 19:18:17 +0800

    第一步

--- backend/src/main/java/com/scan/backend/controller/pk/ScanInfoController.java
+++ backend/src/main/java/com/scan/backend/controller/pk/ScanInfoController.java
@@ -3,18 +3,17 @@ package com.scan.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class ScanInfoController {
     @RequestMapping("getscaninfo/")
-    public List<String> getScanInfo(){
-        List<String>list = new LinkedList<>();
-        list.add("111111");
-        list.add("222222");
-        list.add("333333");
-        return list;
+    public Map<String,String> getScanInfo(){
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","bot");
+        bot1.put("rating","1500");
+        return bot1;
     }
 }
